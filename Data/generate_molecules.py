d2ee85e21a3f0a141ab3f387622488d7a70869dc
eerhan <eerhan@sfu.ca>
2017-11-10 12:22:57 -0800

    Added argparse, added random seed, changed design to be easier to implement in a pipeline

--- Data/generate_molecules.py
+++ Data/generate_molecules.py
@@ -1,22 +1,49 @@
 from pyfaidx import Fasta
-
+import sys
+import argparse
 import random
 import math
 
-molecule_size_mu = 150
-molecule_size_sig = 20
-number_of_molecules = 100
-random_seed = 42
-output_file_path = "molecules.fa"
 
-random.seed(random_seed)
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Generates molecules/amplicons")
+    parser.add_argument("-r", "--reference", type=str, help="Reference genome from which to generate molecules")
+    parser.add_argument("-m", "--molecules", type=str, help="Output molecules/amplicons fasta file (default: stdout)",
+                        default=None)
+    parser.add_argument("-s", "--random-seed", type=int, help="Define a seed (default: no seed)",
+                        default=None)
+    args = parser.parse_args()
+    return args
 
-genome = Fasta("e_coli.fa.gz")
 
-genome_length = len(genome[0])
-with open(output_file_path, 'w+') as output_file:
+def generate_molecule(genome_file_path,
+                      output_file_path=sys.stdout,
+                      molecule_size_mu=150,
+                      molecule_size_sig=20,
+                      number_of_molecules=100,
+                      random_seed=None):
+
+    if output_file_path:
+        output_file = open(output_file_path, 'w+')
+    else:
+        output_file = sys.stdout
+    random.seed(random_seed)
+    genome = Fasta(genome_file_path)
+    genome_length = len(genome[0])
     for i in range(number_of_molecules):
         start = random.randrange(0, genome_length)
-        end = min([start + math.floor(random.normalvariate(mu=molecule_size_mu, sigma=molecule_size_sig)), genome_length])
+        end = min(
+            [start + math.floor(random.normalvariate(mu=molecule_size_mu, sigma=molecule_size_sig)),
+             genome_length])
         print('>'+str(i)+':'+str(start)+'-'+str(end), file=output_file)
         print(genome[0][start:end], file=output_file)
+
+
+def main():
+    args = parse_args()
+    generate_molecule(args.reference, args.molecules, random_seed=args.random_seed)
+
+
+if __name__ == "__main__":
+    main()
