c2b5db5ed1e13ac7e2f4538147b279587267fb80
eerhan <eerhan@sfu.ca>
2017-11-10 12:46:26 -0800

    Added argparse, added random seed, changed design to be easier to implement in a pipeline

--- Data/generate_barcodes.py
+++ Data/generate_barcodes.py
@@ -1,17 +1,42 @@
+import argparse
 import rstr
-
+import sys
 import random
 
-random_seed = 42
-number_of_barcodes = 1000
-barcode_regex = r'[ACGT]{10}'
-output_file_path = "barcodes"
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Generates whitelist of barcodes")
+    parser.add_argument("-n", "--num-of-barcodes", type=int, help="Number of barcodes (default: 1000)",
+                        default=1000)
+    parser.add_argument("-o", "--output", type=str, help="Output barcodes file (default: stdout)")
+    parser.add_argument("-s", "--random-seed", type=int, help="Define a seed (default: no seed)", default=None)
+    args = parser.parse_args()
+    return args
 
 
-barcodes = set()
-while len(barcodes) < number_of_barcodes:
-    barcodes.add(rstr.xeger(barcode_regex))
+def generate_barcodes(output_file_path=None,
+                      random_seed=None,
+                      number_of_barcodes=1000):
+    random.seed(random_seed)
+    barcode_regex = r'[ACGT]{10}'
+    barcodes = set()
+    while len(barcodes) < number_of_barcodes:
+        barcodes.add(rstr.xeger(barcode_regex))
 
-with open(output_file_path, 'w+') as output_file:
+    if output_file_path:
+        output_file = open(output_file_path, 'w+')
+    else:
+        output_file = sys.stdout
+
     for barcode in barcodes:
         print(barcode, file=output_file)
+
+
+def main():
+    args = parse_args()
+    generate_barcodes(output_file_path=args.output, random_seed=args.random_seed,
+                      number_of_barcodes=args.num_of_barcodes)
+
+
+if __name__ == "__main__":
+    main()
