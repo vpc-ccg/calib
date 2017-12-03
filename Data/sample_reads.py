d1a01525d549058a9275ad24be22eb0bc2c175c7
emreerhan <eerhan@gphost04.bcgsc.ca>
2017-12-03 14:18:25 -0800

    pushing to edit on my computer

--- Data/sample_reads.py
+++ Data/sample_reads.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 
-def sample_fastq(fastq_file, choices):
+def sample_fastq(fastq_file, choices, output):
     fastq = open(fastq_file)
+    out = open(output)
     EOF = False
     read_is_next = False
     i = 0
@@ -12,7 +13,8 @@ def sample_fastq(fastq_file, choices):
         if read_is_next:
             read_is_next = False
             if choices[choice_index] == i:
-                print(line)
+                choice_index += 1
+                print(line, file=out)
         if line.startswith("@"):
             i += 1
             read_is_next = True
@@ -24,9 +26,13 @@ def sample_fastq(fastq_file, choices):
 def main():
     sample_size = 10000000
     num_of_reads = 59998500
-    fastq_file = 'amp_seq_10k_1m1.fq'
+    fastq_file1 = 'amp_seq_10k_1m1.fq'
+    fastq_file2 = 'amp_seq_10k_1m2.fq'
     choices = np.random.choice([i for i in range(1, num_of_reads+1)], size=sample_size, replace=False)
-    sample_fastq(fastq_file, choices)
+    print('choices selected')
+    choices = np.sort(choices)
+    sample_fastq(fastq_file1, choices, "amp_seq_10k_1m_sampled1.fq")
+    sample_fastq(fastq_file2, choices, "amp_seq_10k_1m_sampled2.fq")
 
 
 if __name__ == "__main__":
