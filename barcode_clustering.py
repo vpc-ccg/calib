56cb81b529bc132a24630fc41c4da18651c6c983
Baraa Orabi <baraaorabi@gmail.com>
2017-12-03 20:18:32 -0800

    Minimizers now workgit add barcode_clustering.py !

--- barcode_clustering.py
+++ barcode_clustering.py
@@ -18,12 +18,15 @@ def parse_args():
         description="Clusters barcodes by locality sensitive hashing.")
     parser.add_argument("-f", "--forward-reads", type=str, help="Forward read file path.", required=True)
     parser.add_argument("-r", "--reverse-reads", type=str, help="Reverse read file path.", required=True)
+    parser.add_argument("-t", "--barcode-mini-tsv-file", type=str, help="Reverse read file path.", required=True)
     parser.add_argument("-l", "--barcode-length", type=int, default=10,
                         help="Barcode length (default: 10)")
     parser.add_argument("-e", "--error-tolerance", type=int, default=2,
                         help="Error tolerance for barcode clustering (default: 2)")
     parser.add_argument("-m", "--minimizers-count", type=int, default=3,
                             help="Number of minimizers per read mate (default: 3)")
+    parser.add_argument("-x", "--minimizers-threshold", type=int, default=1,
+                            help="Threshold for number of minimizers matching per read mate (default: 1)")
     parser.add_argument("-k", "--k-mer-size", type=int, default=8,
                             help="K-mer size for the minimizers (default: 8)")
     parser.add_argument("-o", "--log-file", help="Log file path.", required=True)
@@ -35,8 +38,9 @@ def parse_args():
 def template_generator(barcode_size, error_tolerance):
     template_id = 0
     for comb in combinations(range(barcode_size), barcode_size - error_tolerance):
+        comb = list(comb)
         def template(barcode):
-            return ''.join([barcode[i] for i in comb])
+            return tuple([barcode[i] for i in comb])
         yield template, template_id
         template_id += 1
 
@@ -134,7 +138,9 @@ def main():
     args = parse_args()
     _barcode_length = args.barcode_length
     _error_tolerance = args.error_tolerance
+    _barcode_mini_tsv = args.barcode_mini_tsv_file
     _minimizer_count = args.minimizers_count
+    _minimizers_threshold = args.minimizers_threshold
     _k_mer_size = args.k_mer_size
 
     log_file = open(args.log_file, 'w+', buffering=1)
@@ -142,77 +148,91 @@ def main():
     print('Step: Extracting barcodes...', file=log_file)
     start_time = time.time()
 
-    # Extracting barcodes from fastq files
-    barcode_lines_1 = get_barcodes(args.forward_reads, _barcode_length)
-    barcode_lines_2 = get_barcodes(args.reverse_reads, _barcode_length)
-    if not len(barcode_lines_1) == len(barcode_lines_2):
-        print('\tYou messed up; the read files are not of the same length', file=log_file)
-        sys.exit(42)
-    # Getting a unique representative from each set of set of identical barcodes
-    barcode_pairs_to_lines = get_barcode_pair_to_line_mapping(barcode_lines_1, barcode_lines_2)
+    node_to_reads_dict = dict()
 
-    print('\tTotal number of unique barcode pairs:', len(barcode_pairs_to_lines), file=log_file)
+    _barcode_mini_tsv_file = open(_barcode_mini_tsv)
+    for idx, line in enumerate(_barcode_mini_tsv_file):
+        node_key = tuple(line.rstrip().split('\t'))
+        if node_key in node_to_reads_dict:
+            node_to_reads_dict[node_key].append(idx)
+        else:
+            node_to_reads_dict[node_key] = [idx]
+
+    node_count = len(node_to_reads_dict)
+    node_to_reads = np.zeros(shape=node_count, dtype=object)
+    node_to_barcode_1 = np.empty(shape=node_count, dtype='S{}'.format(_barcode_length))
+    node_to_barcode_2 = np.empty(shape=node_count, dtype='S{}'.format(_barcode_length))
+    node_to_mini_1 = np.zeros(shape=(node_count, _minimizer_count), dtype=np.int64)
+    node_to_mini_2 = np.zeros(shape=(node_count, _minimizer_count), dtype=np.int64)
+
+    for idx, key in enumerate(node_to_reads_dict):
+        node_to_reads[idx] = node_to_reads_dict[key]
+        node_to_barcode_1[idx] = key[0]
+        node_to_barcode_2[idx] = key[1]
+        node_to_mini_1[idx] = key[2:2+_minimizer_count]
+        node_to_mini_2[idx] = key[2+_minimizer_count:2+_minimizer_count+_minimizer_count]
+    # for idx in range(node_count):
+    #     print(node_to_reads[idx])
+    #     print(node_to_barcode_1[idx], node_to_barcode_2[idx], node_to_mini_1[idx], node_to_mini_2[idx], sep='\t')
+
+    print('\tTotal number of nodes:', node_count, file=log_file)
 
     finish_time = time.time()
     print('\tLast step took {} seconds'.format(finish_time - start_time), file=log_file)
 
-    print('Step: Storing reverse complement of barcodes...', file=log_file)
-    start_time = time.time()
-
-    barcodes_1 = ['']*len(barcode_pairs_to_lines)
-    barcodes_2 = ['']*len(barcode_pairs_to_lines)
-    barcodes_rev_compl_1 = ['']*len(barcode_pairs_to_lines)
-    barcodes_rev_compl_2 = ['']*len(barcode_pairs_to_lines)
-
-    for index, barcode_pair in enumerate(barcode_pairs_to_lines.keys()):
-        barcodes_1[index] = barcode_pair[0]
-        barcodes_2[index] = barcode_pair[1]
-        barcodes_rev_compl_1[index] = reverse_complement(barcode_pair[0])
-        barcodes_rev_compl_2[index] = reverse_complement(barcode_pair[1])
-
-    finish_time = time.time()
-    print('\tLast step took {} seconds'.format(finish_time - start_time), file=log_file)
     print('Step: LSH of barcodes...', file=log_file)
     start_time = time.time()
 
-    # lsh_list = [{} for _ in range(int(nCr(_barcode_length, _error_tolerance)))]
     fake_barcode = ''.join([chr(x+65) for x in range(_barcode_length)])
 
-    barcode_graph_adjacency_sets = [{x} for x in range(len(barcode_pairs_to_lines))]
+    adjacency_sets = [{x} for x in range(node_count)]
 
     for template, template_id in template_generator(_barcode_length, _error_tolerance):
         # TODO: Instead of lsh_list, generate adjacency set from each lsh then remove the lsh.
         lsh = dict() # lsh_list[template_id]
-        print("\tTemplate {} with ID {}".format(template(fake_barcode), template_id), file=log_file)
-        for barcode_num in range(len(barcodes_1)):
-            barcode_1 = template(barcodes_1[barcode_num])
-            barcode_1_rev = template(barcodes_rev_compl_1[barcode_num])
-            barcode_2 = template(barcodes_2[barcode_num])
-            barcode_2_rev = template(barcodes_rev_compl_2[barcode_num])
+        # print("\tTemplate {} with ID {}".format(template(fake_barcode), template_id), file=log_file)
+        for idx in range(node_count):
+            barcode_1 = template(node_to_barcode_1[idx])
+            barcode_2 = template(node_to_barcode_2[idx])
+            key = barcode_1 + barcode_2
+            if key in lsh:
+                lsh[key].append(idx)
+            else:
+                lsh[key] = [idx]
+        for adjacent_nodes in lsh.values():
+            for node in adjacent_nodes:
+                adjacency_sets[node].update(set(adjacent_nodes))
+    # for idx, neighbors in enumerate(adjacency_sets):
+    #     print(idx, neighbors, sep='\t')
+    finish_time = time.time()
+    print('\tLast step took {} seconds'.format(finish_time - start_time), file=log_file)
 
-            new_key = barcode_1 + barcode_2
-            lsh[new_key] = lsh.get(new_key, {barcode_num}).union({barcode_num})
-            new_key = barcode_2_rev + barcode_1_rev
-            lsh[new_key] = lsh.get(new_key, {barcode_num}).union({barcode_num})
-        for val in lsh.values():
-            for node in val:
-                adjacent_nodes = val#.difference({node})
-                barcode_graph_adjacency_sets[node].update(adjacent_nodes)
+    print("Step: Removing edges due to mini\'s...", file=log_file)
+    start_time = time.time()
 
+    for node, neighbors in enumerate(adjacency_sets):
+        for neighbor in list(neighbors):
+            # print(node_to_mini_1[node], node_to_mini_1[neighbor], sep='\t')
+            # print(node_to_mini_2[node], node_to_mini_2[neighbor], sep='\t')
+            # print([node_to_mini_1[node][i]==node_to_mini_1[neighbor][i] for i in range(_minimizer_count)])
+            # print([node_to_mini_2[node][i]==node_to_mini_2[neighbor][i] for i in range(_minimizer_count)])
+            if (sum([node_to_mini_1[node][i]==node_to_mini_1[neighbor][i] for i in range(_minimizer_count)]) < _minimizers_threshold) or \
+            ((sum([node_to_mini_2[node][i]==node_to_mini_2[neighbor][i] for i in range(_minimizer_count)]) < _minimizers_threshold)):
+                neighbors.remove(neighbor)
     finish_time = time.time()
     print('\tLast step took {} seconds'.format(finish_time - start_time), file=log_file)
 
 
-    print("Step: Building barcode graph from adjacency_sets...", file=log_file)
+    print("Step: Building graph from adjacency_sets...", file=log_file)
     start_time = time.time()
 
-    barcode_graph = Graph([(node, neighbor) for node, neighbors in enumerate(barcode_graph_adjacency_sets) for neighbor in neighbors])
+    graph = Graph([(node, neighbor) for node, neighbors in enumerate(adjacency_sets) for neighbor in neighbors])
     print("\tGraph building from adjacency lists is completed", file=log_file)
 
-    barcode_graph.vs['id'] = [x for x in range(len(barcode_pairs_to_lines))]
+    graph.vs['id'] = [x for x in range(node_count)  ]
     print("\tLabeling vertices on graph is completed", file=log_file)
 
-    barcode_graph.simplify()
+    graph.simplify()
     print("\tSimplifying the graph is completed", file=log_file)
 
     finish_time = time.time()
@@ -220,25 +240,26 @@ def main():
     print("Step: Getting clusters (connected components) of the barcode graph...", file=log_file)
     start_time = time.time()
 
-    clusters = barcode_graph.clusters()
+    clusters = graph.clusters()
     print('\tThere total of {} connected components'.format(len(clusters)), file=log_file)
 
-    count = 0
+    
     for index, connected_component in enumerate(clusters):
-        vertices_count = len(connected_component)
+        nodes_count = len(connected_component)
         edges_count = 0
-        for vertex in connected_component:
-            edges_count += len(barcode_graph_adjacency_sets[vertex])
-        edges_count -= vertices_count
+        read_count = 0
+        for node in connected_component:
+            edges_count += len(adjacency_sets[node])
+            read_count += len(node_to_reads[node])
+        edges_count -= nodes_count
         edges_count /= 2
         edges_count = int(edges_count)
-        if vertices_count == 1:
-            print('#{}\t{}\t{}\t{}:'.format(index, vertices_count, edges_count, 'nan'), file=log_file)
+        if nodes_count == 1:
+            print('#{}\t{}\t{}\t{}\t{}:'.format(index, nodes_count, edges_count, read_count,'nan'), file=log_file)
         else:
-            print('#{}\t{}\t{}\t{}:'.format(index,  vertices_count, edges_count, 2*edges_count/(vertices_count * (vertices_count-1))), file=log_file)
-        for barcode in connected_component:
-            print(barcodes_1[barcode], barcodes_2[barcode], file=log_file)
-        count += 1
+            print('#{}\t{}\t{}\t{}\t{}:'.format(index,  nodes_count, edges_count, read_count, 2*edges_count/(nodes_count * (nodes_count-1))), file=log_file)
+        for node in connected_component:
+            print(node_to_barcode_1[node], node_to_barcode_2[node], node_to_mini_1[node], node_to_mini_2[node], file=log_file)
 
     finish_time = time.time()
     print('\tLast step took {} seconds'.format(finish_time - start_time), file=log_file)
