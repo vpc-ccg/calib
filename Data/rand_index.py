1f2ab07ec29be2a52490498266547f8e379cd411
Baraa Orabi <baraaorabi@gmail.com>
2017-12-12 13:24:52 -0800

    Rand Index accuracy script completed

---
+++ Data/rand_index.py
@@ -0,0 +1,68 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Calculates Rand Index for a clusters file")
+    parser.add_argument("-c",
+                        "--input-clusters-file",
+                        type=str,
+                        required=True,
+                        help="Input .clusters file to calculate Rand Index on")
+    parser.add_argument("-m",
+                        "--number-of-true-molecules",
+                        type=int,
+                        required=True,
+                        help="The number of molecules generated in the simulated ground truth.")
+    parser.add_argument("-r",
+                        "--number-of-reads-per-true-molecule",
+                        type=int,
+                        required=True,
+                        help="The number of simulted reads per molecule generated in the simulated ground truth.")
+    args = parser.parse_args()
+    return args
+
+
+def score_cluster(cluster_counts):
+    score = 0
+    cluster_counts = list(cluster_counts.values())
+    for i in range(len(cluster_counts)):
+        score = score + cluster_counts[i]*(cluster_counts[i]-1)//2
+        for j in range(i+1, len(cluster_counts)):
+             score = score - cluster_counts[i]*cluster_counts[j]
+    return score
+
+def get_predicted_cluster(line):
+    line = line.rstrip()
+    line = line.split('\t')
+    line = line[0]
+    line = line.split('_')
+    line = line[2]
+    line = line.split(':')
+    line = line[0]
+    return int(line)
+
+def main():
+    args = parse_args()
+    clusters_file = open(args.input_clusters_file)
+    _molecules_total = args.number_of_true_molecules
+    _reads_per_molecule = args.number_of_reads_per_true_molecule
+    _reads_total = _molecules_total*_reads_per_molecule
+
+    max_possible_TP = (_reads_total - _reads_per_molecule) * _reads_per_molecule * _molecules_total // 2
+    line = clusters_file.readline()
+    while(line != ''):
+        line = clusters_file.readline()
+        cluster_counts = dict()
+        while (line != '' and line[0] != '#'):
+            predicted_cluster = get_predicted_cluster(line)
+            if predicted_cluster in cluster_counts:
+                cluster_counts[predicted_cluster] = cluster_counts[predicted_cluster] + 1
+            else:
+                cluster_counts[predicted_cluster] = 1
+            line = clusters_file.readline()
+        max_possible_TP = max_possible_TP + score_cluster(cluster_counts)
+    print( max_possible_TP/ (_reads_total*(_reads_total-1)//2 ))
+
+
+if __name__ == "__main__":
+    main()
