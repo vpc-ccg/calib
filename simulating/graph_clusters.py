cb35b89afc285aa0a7f82bf8c575d5d8f2d5d4f2
Baraa Orabi <baraaorabi@gmail.com>
2018-03-29 14:34:04 -0700

    Cluster comparison using bipartite graph now prints examples++

--- simulating/graph_clusters.py
+++ simulating/graph_clusters.py
@@ -55,7 +55,7 @@ def main():
         line = line.rstrip().split('\t')
         tcid = tcid_counter
         rid = int(line[1])
-
+        reads[rid] = str(line[0])
         if tcid in tcid_to_rid_set:
             tcid_to_rid_set[tcid].add(rid)
         else:
@@ -70,10 +70,11 @@ def main():
         line = line.replace(' ', '\t')
         line = line.rstrip().split('\t')
         rid = int(line[1])
-        reads[rid] = line[3]+'\t'+line[6]
         tcid = rid_to_tcid[rid]
 
         rid_to_pcid[rid] = pcid_counter
+        reads[rid] = line[3]+'\t'+line[6]+'\t'+str(rid)+'\t'+reads[rid]+'\t'+str(rid_to_pcid[rid])+'\t'+str(rid_to_tcid[rid])
+        # print(reads[rid])
         if pcid_counter in pcid_to_rid_set:
             pcid_to_rid_set[pcid_counter].add(rid)
         else:
@@ -92,38 +93,52 @@ def main():
         key = tuple([tcid, pcid_counter])
         contigency_matrix[key] = contigency_matrix.get(key, 0) + 1
 
-    if (args.output_accuracy_results):
-        results = open(args.output_accuracy_results+'.ari', 'w+')
-    else:
-        results = sys.stdout
+    # if (args.output_accuracy_results):
+    #     results = open(args.output_accuracy_results+'.ari', 'w+')
+    # else:
+    #     results = sys.stdout
 
-    Y = []
-    X = []
-    lonely_read_cluster_counter = -1
-    for rid in rid_to_tcid:
-        Y.append(rid_to_tcid[rid])
-        if rid in rid_to_pcid:
-            X.append(rid_to_pcid[rid])
-        else:
-            X.append(lonely_read_cluster_counter)
-            lonely_read_cluster_counter -= 1
+    # Y = []
+    # X = []
+    # lonely_read_cluster_counter = -1
+    # for rid in rid_to_tcid:
+    #     Y.append(rid_to_tcid[rid])
+    #     if rid in rid_to_pcid:
+    #         X.append(rid_to_pcid[rid])
+    #     else:
+    #         X.append(lonely_read_cluster_counter)
+    #         lonely_read_cluster_counter -= 1
     pcid_to_pcidx = dict()
     tcid_to_tcidx = dict()
     pcidx_to_pcid = dict()
     tcidx_to_tcid = dict()
     node_count = 0
+
+    pcid_singltons = set()
+    tcid_singltons = set()
+    for rid in rid_to_tcid:
+        pcid = rid_to_pcid[rid]
+        tcid = rid_to_tcid[rid]
+        if len(tcid_to_rid_set[tcid]) == 1 and len(pcid_to_rid_set[pcid]) == 1:
+            tcid_singltons.add(tcid)
+            pcid_singltons.add(pcid)
+
     for pcid in pcid_to_tcid_set:
+        # if pcid in pcid_singltons:
+        #     pass
         pcidx_to_pcid[node_count] = pcid
         pcid_to_pcidx[pcid] = node_count
         node_count += 1
     for tcid in tcid_to_pcid_set:
+        # if tcid in tcid_singltons:
+        #     pass
         tcidx_to_tcid[node_count] = tcid
         tcid_to_tcidx[tcid] = node_count
         node_count += 1
 
-    max_weight = 0
-    pc_size = len(pcid_to_tcid_set)
-    tc_size = len(tcid_to_pcid_set)
+    weights = set()
+    pc_size = len(pcidx_to_pcid)
+    tc_size = len(tcidx_to_tcid)
 
     adjacency = [None]*node_count
 
@@ -132,34 +147,46 @@ def main():
         for tcidx in neighbors:
             weight = len(pcid_to_rid_set[pcidx_to_pcid[pcidx]].intersection(tcid_to_rid_set[tcidx_to_tcid[tcidx]]))
             neighbors[tcidx] = weight
-            if weight > max_weight:
-                max_weight = weight
+            weights.add(weight)
         adjacency[pcidx] = neighbors
     for tcidx in range(pc_size, node_count):
         neighbors = {pcid_to_pcidx[pcid] : 0 for pcid in tcid_to_pcid_set[tcidx_to_tcid[tcidx]]}
         for pcidx in neighbors:
             # weight =
             neighbors[pcidx] = len(tcid_to_rid_set[tcidx_to_tcid[tcidx]].intersection(pcid_to_rid_set[pcidx_to_pcid[pcidx]]))
-            if neighbors[pcidx] > max_weight:
-                max_weight = neighbors[pcidx]
+            weights.add(weight)
         adjacency[tcidx] = neighbors
 
-    for current_weight in range(max_weight+2):
+    for current_weight in sorted((weights)):
         visited = [False]*node_count
         shared_cc = 0
         pc_cc = 0
         tc_cc = 0
+        cc_composition = dict()
+        cc_examples = dict()
         for idx in range(node_count):
             # print (idx, visited[idx])
             if visited[idx]:
                 continue
-            cc_size = 1
+            cc_size = 0
+            cc_rid_list = list()
+            cc_pc_count = 0
+            cc_tc_count = 0
             visited[idx] = True
             opened = [idx]
             # print(opened)
             while len(opened) > 0:
-                # print (opened)
+                # print (idx, opened)
                 current_node = opened.pop()
+                if current_node < pc_size:
+                    cc_rid_list.extend(list(pcid_to_rid_set[pcidx_to_pcid[current_node]]))
+                    cc_pc_count += 1
+                else:
+                    cc_rid_list.extend(list(tcid_to_rid_set[tcidx_to_tcid[current_node]]))
+                    cc_tc_count += 1
+                # print(opened)
+                cc_size += 1
+
                 # print (adjacency[current_node], type(adjacency[current_node]))
                 for neighbor in adjacency[current_node]:
                     weight = adjacency[current_node][neighbor]
@@ -167,7 +194,6 @@ def main():
                     if weight >= current_weight and visited[neighbor] != True:
                         visited[neighbor] = True
                         opened.append(neighbor)
-                        cc_size += 1
             if cc_size ==  1:
                 if idx < pc_size:
                     pc_cc += 1
@@ -175,7 +201,24 @@ def main():
                     tc_cc += 1
             else:
                 shared_cc += 1
-        print(current_weight, pc_cc, tc_cc, shared_cc, sep='\t')
+            rid_count = 0
+            cc_rid_list.sort()
+            cc_rid_list_unique = list()
+            # print(cc_rid_list)
+            for index, rid in enumerate(cc_rid_list):
+                if index == len(cc_rid_list) - 1 or rid != cc_rid_list[index+1]:
+                    cc_rid_list_unique.append(rid)
+            # print(rid_count)
+            key = (cc_pc_count, cc_tc_count, len(cc_rid_list_unique))
+            if key in cc_composition:
+                cc_composition[key] += 1
+            else:
+                cc_composition[key] = 1
+                cc_examples[key] = '\n'.join((reads[rid] for rid in cc_rid_list_unique))
+
+        for key in sorted(cc_composition):
+            print('#',int(current_weight), int(key[0]), int(key[1]), int(key[2]), int(cc_composition[key]), sep='\t')
+            print(cc_examples[key])
 
 if __name__ == "__main__":
     main()
