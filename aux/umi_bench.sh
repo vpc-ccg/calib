b62845ebbdaf7f3c184d78314df965b0dfbe2d6b
Baraa Orabi <baraaorabi@gmail.com>
2018-04-27 10:13:25 -0700

    Converting UMI-tools output to cluster format

--- aux/umi_bench.sh
+++ aux/umi_bench.sh
@@ -35,7 +35,7 @@ BEGIN{OFS = "\t"; n = 0}
 "$samtools_path"samtools sort -@ $2 -O bam -m 2G -T samtemp -o "$base".extracted.trim.mapping.sorted.bam "$base".extracted.trim.mapping.renamed.sam
 "$samtools_path"samtools index "$base".extracted.trim.mapping.sorted.bam
 umi_tools group -I "$base".extracted.trim.mapping.sorted.bam --group-out "$base".extracted.trim.mapping.sorted.cluster.tsv --paired --method cluster --edit-distance-threshold 2 --soft-clip-threshold 9
-
+awk 'BEGIN{prev = -1} NR > 1 {if ($9 != prev) {prev = $9; print "# "$9} rid = substr($1, 1,length($1)-17); print $2"."$3,rid,"@"rid,"s1","q1","@"rid,"s2","q2" }' "$base".extracted.trim.mapping.sorted.cluster.tsv > "$base".extracted.trim.mapping.sorted.cluster
 
 "$samtools_path"samtools view -f 64  "$base".extracted.trim.mapping.renamed.sam | awk '{print "@"substr($1,1,length($1)-17)"\n"substr($1, length($1) - 15  , 8)$10"\n+\nKKKKKKKK"$11}' > "$base".extracted.trim.mapping.R1.fastq
 "$samtools_path"samtools view -f 128 "$base".extracted.trim.mapping.renamed.sam | awk '{print "@"substr($1,1,length($1)-17)"\n"substr($1, length($1) - 15+8, 8)$10"\n+\nKKKKKKKK"$11}' > "$base".extracted.trim.mapping.R2.fastq
