fe8af4ba9f549d13389770c5160c7ef691924663
Baraa Orabi <baraaorabi@gmail.com>
2018-04-27 18:37:36 -0700

    Du Novo test merged with other tests; Added ratio to starcode tests

--- run_tests.sh
+++ run_tests.sh
@@ -20,6 +20,9 @@ do
     num_molecules=10
     ;;
   esac
+  # Initializing the expirement by creating the log files and dataset
+  ./benchmark make_log_files simulate
+
   # calib_log
   ./benchmark calib_log reference_name=hg38 bed=Panel.hg38 barcode_error_tolerance=1 kmer_size=8 minimizers_num=5 minimizers_threshold=2 num_molecules=$num_molecules num_barcodes=$num_barcodes
   ./benchmark calib_log reference_name=hg38 bed=Panel.hg38 barcode_error_tolerance=1 kmer_size=4 minimizers_num=5 minimizers_threshold=2 num_molecules=$num_molecules num_barcodes=$num_barcodes
@@ -27,9 +30,12 @@ do
   ./benchmark calib_log reference_name=hg38 bed=Panel.hg38 barcode_error_tolerance=2 kmer_size=4 minimizers_num=5 minimizers_threshold=2 num_molecules=$num_molecules num_barcodes=$num_barcodes
 
   # starcode_log
-  for starcode_dist in 1 2 3 4
+  for starcode_dist in 4 3 2 1
   do
-    ./benchmark starcode_log reference_name=hg38 bed=Panel.hg38 num_molecules=$num_molecules num_barcodes=$num_barcodes starcode_dist=$starcode_dist
+      for starcode_ratio in 1 2 3 4 5
+      do
+          ./benchmark starcode_log reference_name=hg38 bed=Panel.hg38 num_molecules=$num_molecules num_barcodes=$num_barcodes starcode_dist=$starcode_dist starcode_ratio=$starcode_ratio
+      done
   done
   # rainbow_log
   for rainbow_mismatch in 1 2 3 4 5 6 7 8 9
@@ -37,8 +43,11 @@ do
     ./benchmark rainbow_log reference_name=hg38 bed=Panel.hg38 num_molecules=$num_molecules num_barcodes=$num_barcodes rainbow_mismatch=$rainbow_mismatch
     ./benchmark rainbow_log reference_name=hg38 bed=Panel.hg38 num_molecules=$num_molecules num_barcodes=$num_barcodes rainbow_mismatch=$rainbow_mismatch rainbow_div=true
   done
+  # du novo
+  for dunovo_dist in 1 2 3 4
+  do
+    ./benchmark dunovo_log dunovo_prefix="$dunovo_path" reference_name=hg38 bed=Panel.hg38 num_molecules=$num_molecules num_barcodes=$num_barcodes dunovo_dist=$dunovo_dist
+  done
 
 done
 exit
-
-# ./benchmark calib_log rainbow=~/projects/calib/tools/rainbow_2.0.4/rainbow starcode=~/projects/calib/tools/starcode/starcode art_illumina=~/bin/art_bin_MountRainier/art_illumina reference_name=hg38 bed=RCCPanelV2.hg38 pcr_error_rate=0.00005 num_molecules=1000000 pcr_duplication_rate=0.6 num_barcodes=5000 barcode_length=8 minimizers_num=5 minimizers_threshold=3 kmer_size=4 rainbow_mismatch?=3
