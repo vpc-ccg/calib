1e5024f1d18063f83da2ae08c772e2c17eef788c
Baraa Orabi <baraaorabi@gmail.com>
2018-08-03 13:42:57 -0700

    Code refactoring; molecule name format change

--- simulating/generate_molecules.py
+++ simulating/generate_molecules.py
@@ -1,6 +1,4 @@
 from pyfaidx import Fasta
-import pandas as pd
-import numpy as np
 import sys
 import argparse
 import random
@@ -50,89 +48,21 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-
-def generate_random_molecule(mu, sigma, genome_length, chromosome_ends, min_molecule_size):
-    start = random.randrange(0, genome_length)
-    chromosome_indices = np.array(list(chromosome_ends.values()))
-    distances = chromosome_indices - start
-    chrom_end = np.min(distances[distances > 0])
-    chromosome = np.where(distances == chrom_end)[0][0]
-
-    end = min(
-        [start + abs(math.floor(random.normalvariate(mu=mu, sigma=sigma))),
-         chrom_end])
-    while end - start < 0:
-        end = min(
-            [start + abs(math.floor(random.normalvariate(mu=mu, sigma=sigma))),
-             genome_length])
-    if end - start < min_molecule_size:
-        start = end - min_molecule_size
-    return chromosome, start, end
-
-
-def process_genome(genome):
-    # genome is a pyfaidx.Fasta object
-    chromosome_strings = [str(genome[chrom_id]) for chrom_id in genome.keys()]
-    chromosome_ids = list(genome.keys())
-    hg38_chromosomes = [str(i) for i in range(1, 23)] + ['Y', 'X']
-    genome_str = "".join(chromosome_strings)
-    chromosome_indices = np.array([len(chrom) for chrom in chromosome_strings])
-    running_sum = 0
-    chromosome_ends = {}
-    for chrom_id, chrom_length in zip(chromosome_ids, chromosome_indices):
-        if chrom_id in hg38_chromosomes:
-            chrom_id = 'chr' + chrom_id
-        running_sum += chrom_length
-        chromosome_ends[chrom_id] = running_sum
-    chromosome_ids = list(chromosome_ends.keys())
-    genome_length = len(genome_str)
-    return chromosome_ids, chromosome_ends, genome_str, genome_length
-
-
-def generate_random_molecules(genome_file_path,
-                              output_file_path=None,
-                              molecule_size_mean=150,
-                              molecule_size_standard_dev=20,
-                              number_of_molecules=100,
-                              random_seed=None,
-                              min_molecule_size=150):
-
-    if output_file_path:
-        output_file = open(output_file_path, 'w+')
-    else:
-        output_file = sys.stdout
-    random.seed(random_seed)
-    genome = Fasta(genome_file_path)
-    chromosome_ids, chromosome_ends, genome_str, genome_length = process_genome(genome)
-
-    for i in range(number_of_molecules):
-        chromosome_idx, start, end = generate_random_molecule(molecule_size_mean,
-                                                              molecule_size_standard_dev,
-                                                              genome_length,
-                                                              chromosome_ends,
-                                                              min_molecule_size)
-        chromosome = chromosome_ids[chromosome_idx]
-        print(">{}:{}-{}-{}".format(i, chromosome, start, end), file=output_file)
-        print(genome_str[start:end], file=output_file)
-
 def generate_molecules_from_bed(genome_file_path,
                                 bed_file_path,
-                                output_file_path=None,
-                                molecule_size_mean=150,
-                                molecule_size_standard_dev=20,
-                                number_of_molecules=100,
-                                random_seed=None,
-                                min_molecule_size=150):
-    if output_file_path:
-        output_file = open(output_file_path, 'w+')
-    else:
-        output_file = sys.stdout
+                                molecule_size_mean,
+                                molecule_size_standard_dev,
+                                number_of_molecules,
+                                random_seed,
+                                min_molecule_size,
+                                output_file_path=None):
     random.seed(random_seed)
     genome_obj = Fasta(genome_file_path)
     genome = dict()
     for key in genome_obj.keys():
         genome[key] = str(genome_obj[key])
-    bed_positions = list()
+    del genome_obj
+    bed_positions = set()
     for line in open(bed_file_path).readlines()[1:]:
         line = line.rstrip().split('\t')
         chr = line[0]
@@ -140,47 +70,48 @@ def generate_molecules_from_bed(genome_file_path,
         end = int(line[2])
         gene = line[3]
         for idx in range(start, end + 1):
-            bed_positions.append((chr, idx))
+            bed_positions.add((chr, idx, gene))
+    bed_positions = list(bed_positions)
 
-    num_molecules = 0
-    while num_molecules < number_of_molecules:
+    molecules = list()
+    while len(molecules) < number_of_molecules:
         mol_len = math.floor(random.normalvariate(mu=molecule_size_mean, sigma=molecule_size_standard_dev))
         if mol_len < min_molecule_size:
             continue
-        bed_chr, bed_pos = random.choice(bed_positions)
-        if bed_pos - mol_len <= 0:
+        chr, pos, gene = random.choice(bed_positions)
+        start = pos - math.floor(random.normalvariate(mu=mol_len/2, sigma=15))
+        end   = pos + mol_len
+        if start <= 0 or end > len(genome[chr]):
             continue
-        chr_len = len(genome[bed_chr])
-        if bed_pos + mol_len >= chr_len:
-            continue
-        mol_start = bed_pos - random.randint(0, mol_len)
-        mol_end = mol_start + mol_len
-        mol_str = str(genome[bed_chr][mol_start: mol_end]).upper()
-        if 'N' in mol_str:
-            continue
-        print(">{}:{}-{}-{}".format(num_molecules, bed_chr, mol_start, mol_end), file=output_file)
-        print(mol_str, file=output_file)
-        num_molecules += 1
+        molecules.append((chr, gene, start, end))
+
+    if output_file_path:
+        output_file = open(output_file_path, 'w+')
+    else:
+        output_file = sys.stdout
+
+    molecules.sort()
+    for idx, molecule in enumerate(molecules):
+        chr   = molecule[0]
+        gene  = molecule[1]
+        start = molecule[2]
+        end   = molecule[3]
+        seq = str(genome[chr][start: end]).upper()
+
+        print(">{}:{}:{}:{}:{}".format(idx, chr, gene, start, end), file=output_file)
+        print(seq, file=output_file)
 
 def main():
     args = parse_args()
     if args.bed:
         generate_molecules_from_bed(genome_file_path=args.reference,
                                     bed_file_path=args.bed,
-                                    output_file_path=args.output_molecules,
                                     molecule_size_mean=args.molecule_size_mean,
                                     molecule_size_standard_dev=args.molecule_size_standard_dev,
                                     number_of_molecules=args.number_of_molecules,
                                     random_seed=args.random_seed,
-                                    min_molecule_size=args.min_molecule_size)
-    else:
-        generate_random_molecules(genome_file_path=args.reference,
-                                  output_file_path=args.output_molecules,
-                                  molecule_size_mean=args.molecule_size_mean,
-                                  molecule_size_standard_dev=args.molecule_size_standard_dev,
-                                  number_of_molecules=args.number_of_molecules,
-                                  random_seed=args.random_seed,
-                                  min_molecule_size=args.min_molecule_size)
+                                    min_molecule_size=args.min_molecule_size,
+                                    output_file_path=args.output_molecules)
 
 
 if __name__ == "__main__":
