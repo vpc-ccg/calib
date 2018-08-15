63a2363c26f87c2391ee9e35fe8fdf00b3b95a83
Baraa Orabi <baraaorabi@gmail.com>
2018-08-15 12:06:47 -0700

    Installing UMI-tools in Makefile

--- aux/run_umi-tools.sh
+++ aux/run_umi-tools.sh
@@ -1,41 +1,25 @@
-base=$1
-t=$2
-ref=$3
-samtools_path=$4
+fq_1=$1
+fq_2=$2
+out_dir=$3
+working_dir=$4
+bwa=$5
+ref=$6
+samtools=$7
 
-umi_tools extract -I "$base".R1.fastq --bc-pattern=NNNNNNNN --bc-pattern2=NNNNNNNN --read2-in "$base".R2.fastq --stdout "$base".R1.extracted.fastq  --read2-out "$base".R2.extracted.fastq
+mkdir -p $working_dir
+umi_tools extract \
+    -I $fq_1 --read2-in $fq_2 \
+    --bc-pattern=NNNNNNNN --bc-pattern2=NNNNNNNN \
+    --stdout "$working_dir/barcode-extracted.1.fastq"  --read2-out "$working_dir/barcode-extracted.2.fastq"
 
-awk '{if (NR %2 == 0) {print substr($0,2)} else {print}}' "$base".R1.extracted.fastq > "$base".R1.extracted.trim.fastq
-awk '{if (NR %2 == 0) {print substr($0,2)} else {print}}' "$base".R2.extracted.fastq > "$base".R2.extracted.trim.fastq
-
-bwa mem -t $2 $3 "$base".R1.extracted.trim.fastq "$base".R2.extracted.trim.fastq > "$base".extracted.trim.sam
-
-"$samtools_path"samtools view -F 3852 -h "$base".extracted.trim.sam | \
-   awk 'substr($0,1,1)=="@" || $5 > 0' | \
-   awk '
-BEGIN{OFS = "\t"; n = 0}
-{
-   if(substr($0,1,1)=="@") {
-       print;
-   } else if ($7=="=") {
-       key = $1;
-       $1 = substr($1,length($1)-16);
-       if (key in first_mates) {
-           print n""first_mates[key];
-           print n""$0;
-           n++;
-       } else {
-           first_mates[key] = $0;
-       }
-   }
-}' > \
-   "$base".extracted.trim.mapping.renamed.sam
-
-
-"$samtools_path"samtools sort -@ $2 -O bam -m 2G -T samtemp -o "$base".extracted.trim.mapping.sorted.bam "$base".extracted.trim.mapping.renamed.sam
-"$samtools_path"samtools index "$base".extracted.trim.mapping.sorted.bam
-umi_tools group -I "$base".extracted.trim.mapping.sorted.bam --group-out "$base".extracted.trim.mapping.sorted.cluster.tsv --paired --method cluster --edit-distance-threshold 2 --soft-clip-threshold 9
-awk 'BEGIN{prev = -1} NR > 1 {if ($9 != prev) {prev = $9; print "# "$9} rid = substr($1, 1,length($1)-17); print $2"."$3,rid,"@"rid,"s1","q1","@"rid,"s2","q2" }' "$base".extracted.trim.mapping.sorted.cluster.tsv > "$base".extracted.trim.mapping.sorted.cluster
-
-"$samtools_path"samtools view -f 64  "$base".extracted.trim.mapping.renamed.sam | awk '{print "@"substr($1,1,length($1)-17)"\n"substr($1, length($1) - 15  , 8)$10"\n+\nKKKKKKKK"$11}' > "$base".extracted.trim.mapping.R1.fastq
-"$samtools_path"samtools view -f 128 "$base".extracted.trim.mapping.renamed.sam | awk '{print "@"substr($1,1,length($1)-17)"\n"substr($1, length($1) - 15+8, 8)$10"\n+\nKKKKKKKK"$11}' > "$base".extracted.trim.mapping.R2.fastq
+bwa mem -t 32 $ref "$working_dir/barcode-extracted.1.fastq" "$working_dir/barcode-extracted.2.fastq" | \
+    awk 'substr($0,1,1) == "@" || $2 < 256' | \
+    $samtools sort - -O BAM -@ 32 -m 2G -o "$working_dir/barcode-extracted.bam"
+$samtools index "$working_dir/barcode-extracted.bam"
+umi_tools group  \
+    --paired \
+    --method cluster \
+    --edit-distance-threshold 2 \
+    --soft-clip-threshold 9 \
+    -I "$working_dir/barcode-extracted.bam" \
+    --group-out "$out_dir/umi-tools.cluster.tsv"
