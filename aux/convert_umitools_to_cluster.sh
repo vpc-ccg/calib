cf5ab7f4f74751d889368d392f6b801b618af9b9
Baraa Orabi <baraaorabi@gmail.com>
2018-08-17 14:35:23 -0700

    Fixed bug in UMI-tools to cluster conversion

--- aux/convert_umitools_to_cluster.sh
+++ aux/convert_umitools_to_cluster.sh
@@ -5,6 +5,6 @@ awk '
     }
     NR > 1 {
         rid=int(substr($1, 1, match($1, "_") - 1));
-        print rid,"nid",$9,"n1","s1","q1","n2","s2","q2";
+        print $9,"nid",rid,"n1","s1","q1","n2","s2","q2";
     }
     ' $1
